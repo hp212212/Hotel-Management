38d6d3bc3515b64a1f34a7094602e61ea93d813e
Hitesh <hp212212212@gmail.com>
2023-02-28 15:08:31 -0700

    Reserched about How to Get Available Rooms In Extra.js - 8

--- src/Components/Extra.js
+++ src/Components/Extra.js
@@ -2,10 +2,14 @@ import React from 'react'
 import '../Css/Extra.css'
 import dayjs from 'dayjs'
 import { useSelector } from 'react-redux'
-import relativeTime from 'dayjs/plugin/relativeTime'
+// import relativeTime from 'dayjs/plugin/relativeTime'
+import { GetRoomList } from '../Server/Services';
+
 // var relativeTime = require('dayjs/plugin/relativeTime')
-dayjs.extend(relativeTime)
+// dayjs.extend(relativeTime)
 export default function Extra() {
+  const state = useSelector((state) => state.MainReduser)
+  const RoomList = GetRoomList()
   // const state = useSelector((state) => state.MainReduser)
   // var now = dayjs().format('YYYY-MM-DD')
   // const aaa = state[0].checkin
@@ -20,17 +24,44 @@ export default function Extra() {
   // console.log(now.diff(aaa, 'day'))
   // const b = a.add(7, 'day').format('YYYY-MM-DD')
   let kaka = [1, 2, 3, 4, 5]
-  const hahahaah =() => {
-
-    if (6 in kaka) {
-      alert("Hitesh")
+  const hahahaah = () => {
+    let Rooms = []
+    for (let i of RoomList) {
+      if (document.getElementById("Room").value === i.type) {
+        Rooms = i.rooms
+      }
     }
+    console.log(Rooms)
+    const date1 = dayjs(dayjs(document.getElementById("d1").value).format('YYYY-MM-DD'))
+    const date2 = dayjs(dayjs(document.getElementById("d2").value).format('YYYY-MM-DD'))
+    const Room = document.getElementById("Room").value
+    let TotalDays = date2.diff(date1, 'day')
+    let CheckDay = ''
+    let DeleteRooms = []
+    for (let j of state) {
+      if ((j.status === "Reservation" || j.status === "In House") && j.roomtype === Room) {
+        for (let i = 0; i < TotalDays; i++) {
+          CheckDay = date1.add(i, 'day').format('YYYY-MM-DD')
+          if (CheckDay >= j.checkin && CheckDay < j.checkout) {
+            DeleteRooms.push(j.roomno)
+            Rooms.splice(Rooms.indexOf(j.roomno), 1)
+            // alert(j.roomno)
+          }
+        }
+      }
+    }
+    console.log(Rooms)
+    console.log(DeleteRooms)
   }
 
 
   return (
     <>
       <div className='Main'> Jay Swaminarayan</div>
+      <input id="d1" placeholder='Date - 1' />
+      <input id="d2" placeholder='Date - 2' />
+      <input id="Room" placeholder='Room Type' />
+
       <button className='buttonn' onClick={hahahaah}>Slide</button>
 
 
